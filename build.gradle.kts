ba3e4e0f5bf1015da491d057890f7c8de19b7f4f
jkeller <jkeller@newrelic.com>
2019-08-20 13:47:22 -0700

    Refactor gradle deployment script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-import com.bmuschko.gradle.nexus.NexusPluginExtension
-
 buildscript {
     dependencies {
         classpath("gradle.plugin.com.github.sherter.google-java-format:google-java-format-gradle-plugin:0.8")
-        classpath ("com.bmuschko:gradle-nexus-plugin:2.3.1")
     }
 }
 
@@ -25,8 +22,9 @@ allprojects {
 plugins {
     id("com.github.sherter.google-java-format") version "0.8"
     `java-library`
+    `maven-publish`
+    signing
 }
-apply(plugin = "com.bmuschko.nexus")
 
 googleJavaFormat {
     exclude(".**")
@@ -34,7 +32,7 @@ googleJavaFormat {
 
 dependencies {
     api("io.micrometer:micrometer-core:1.2.0")
-    api("com.newrelic.telemetry:telemetry:0.2.0-SNAPSHOT")
+    api("com.newrelic.telemetry:telemetry:0.2.1-SNAPSHOT")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
@@ -49,24 +47,73 @@ jar.apply {
     manifest.attributes["Implementation-Vendor"] = "New Relic, Inc"
 }
 
-val sonatype by configurations.creating {
-    extendsFrom(configurations["archives"])
-}
-
-val uploadSonatype by tasks.registering(org.gradle.api.tasks.Upload::class) {
-    configuration = configurations["sonatype"]
-
-    isUploadDescriptor = true
-}
-
-configure<NexusPluginExtension> {
-    if (project.properties["useLocalSonatype"] == "true") {
-        sign = false
-        snapshotRepositoryUrl = "http://admin:admin123@localhost:8081/repository/maven-snapshots/"
-        repositoryUrl = "http://admin:admin123@localhost:8081/repository/maven-releases/"
+tasks {
+    val taskScope = this
+    val sources = sourceSets
+    val sourcesJar by creating(Jar::class) {
+        dependsOn(JavaPlugin.CLASSES_TASK_NAME)
+        archiveClassifier.set("sources")
+        from(sources.main.get().allSource)
     }
-    setConfiguration(sonatype)
-}
 
-val build by tasks.named("build")
-build.dependsOn("javadoc")
+    val javadocJar by creating(Jar::class) {
+        dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
+        archiveClassifier.set("javadoc")
+        from(taskScope.javadoc)
+    }
+}
+val useLocalSonatype = project.properties["useLocalSonatype"] == "true"
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name.set(project.name)
+                description.set("Micrometer registry implementation that sends data to New Relic as dimensional metrics")
+                url.set("https://github.com/newrelic/micrometer-registry-newrelic")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("newrelic")
+                        name.set("New Relic")
+                        email.set("opensource@newrelic.com")
+                    }
+                }
+                scm {
+                    url.set("git@github.com:newrelic/micrometer-registry-newrelic.git")
+                    connection.set("scm:git@github.com:newrelic/micrometer-registry-newrelic.git")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            if (useLocalSonatype) {
+                val releasesRepoUrl = uri("http://localhost:8081/repository/maven-releases/")
+                val snapshotsRepoUrl = uri("http://localhost:8081/repository/maven-snapshots/")
+                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            }
+            else {
+                val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                configure<SigningExtension> {
+                    sign(publications["mavenJava"])
+                }
+            }
+            credentials {
+                username = project.properties["sonatypeUsername"] as String?
+                password = project.properties["sonatypePassword"] as String?
+            }
+        }
+    }
+}
