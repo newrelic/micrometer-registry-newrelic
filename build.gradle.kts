d970bf0e2f8c66105092dbd6179163f8de26032b
gfuller <gfuller@newrelic.com>
2019-12-10 15:37:01 -0800

    update dependencies to avoid Deserialization of Untrusted Data vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,14 +36,14 @@ dependencies {
     //note: these are transitive dependencies from micrometer, but needed to be upgraded due to a security vulnerability
     implementation("com.fasterxml.jackson.core:jackson-core:2.9.9")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.9.9")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.10.1")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.26")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
     testImplementation("org.mockito:mockito-core:3.0.0")
     testImplementation("org.mockito:mockito-junit-jupiter:3.0.0")
-    testImplementation("org.mock-server:mockserver-netty:5.6.0")
+    testImplementation("org.mock-server:mockserver-netty:5.7.1")
 }
 
 val jar: Jar by tasks
