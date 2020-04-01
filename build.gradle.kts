189d7caa122e934c205d53a883447dbfa79690d8
jkeller <jkeller@newrelic.com>
2020-04-01 12:03:34 -0700

    Update telemetry-sdk to 0.4.0 and bump micrometer-registry-new-relic version to 0.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ googleJavaFormat {
 
 dependencies {
     api("io.micrometer:micrometer-core:1.3.3")
-    api("com.newrelic.telemetry:telemetry:0.3.4")
+    api("com.newrelic.telemetry:telemetry:0.4.0")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.30")
