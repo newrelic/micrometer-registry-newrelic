d20edcf72a28760df0fed4f5df464099fd955a51
Jeff Alder <49817386+jeffalder@users.noreply.github.com>
2020-07-28 09:14:52 -0700

    Configure publishing tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,27 @@
-buildscript {
-    dependencies {
-        classpath("gradle.plugin.com.github.sherter.google-java-format:google-java-format-gradle-plugin:0.8")
-    }
-}
-
-allprojects {
-    group = "com.newrelic.telemetry"
-    version = project.findProperty("releaseVersion") as String
-    repositories {
-        mavenCentral()
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
-    }
-    tasks.withType<Test> {
-        useJUnitPlatform()
-        testLogging {
-            events("passed", "skipped", "failed")
-        }
-    }
+repositories {
+    mavenCentral()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 plugins {
-    id("com.github.sherter.google-java-format") version "0.8"
+    id("com.github.sherter.google-java-format") version "0.9"
     `java-library`
     `maven-publish`
     signing
+}
+
+group = "com.newrelic.telemetry"
+
+// -Prelease=true will render a non-snapshot version
+// All other values (including unset) will render a snapshot version.
+val release: String? by project
+version = project.findProperty("releaseVersion") as String + if("true" == release) "" else "-SNAPSHOT"
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
 
 googleJavaFormat {
@@ -43,35 +41,28 @@ dependencies {
     testImplementation("org.mock-server:mockserver-netty:5.10.0")
 }
 
-val jar: Jar by tasks
-jar.apply {
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.jar {
+    from ("LICENSE.md")
     manifest.attributes["Implementation-Version"] = project.version
     manifest.attributes["Implementation-Vendor"] = "New Relic, Inc"
 }
 
-tasks {
-    val taskScope = this
-    val sources = sourceSets
-    val sourcesJar by creating(Jar::class) {
-        dependsOn(JavaPlugin.CLASSES_TASK_NAME)
-        archiveClassifier.set("sources")
-        from(sources.main.get().allSource)
-    }
-
-    val javadocJar by creating(Jar::class) {
-        dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
-        archiveClassifier.set("javadoc")
-        from(taskScope.javadoc)
-    }
-}
 val useLocalSonatype = project.properties["useLocalSonatype"] == "true"
 
-configure<PublishingExtension> {
+publishing {
     publications {
-        create<MavenPublication>("mavenJava") {
+        register("mavenJava", MavenPublication::class) {
             from(components["java"])
-            artifact(tasks["sourcesJar"])
-            artifact(tasks["javadocJar"])
+        }
+        // customize all publications here
+        withType(MavenPublication::class) {
             pom {
                 name.set(project.name)
                 description.set("Micrometer registry implementation that sends data to New Relic as dimensional metrics")
@@ -108,16 +99,21 @@ configure<PublishingExtension> {
                 val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
                 val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
                 url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-                configure<SigningExtension> {
-                    sign(publications["mavenJava"])
-                }
             }
             credentials {
-                username = project.properties["sonatypeUsername"] as String?
-                password = project.properties["sonatypePassword"] as String?
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
             }
         }
     }
+}
+
+signing {
+    val signingKeyId: String? by project
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications["mavenJava"])
 }
 
 // This makes it difficult to use modern Java and produce usable output.
